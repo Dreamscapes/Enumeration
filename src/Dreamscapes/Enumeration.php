80caf424b471bed1e23a943583331f5fed1cbfeb
Robert Rossmann <rr.rossmann@me.com>
2014-07-09 22:48:48 +0200

    [dev] Follow PSR-2 coding style

--- src/Dreamscapes/Enumeration.php
+++ src/Dreamscapes/Enumeration.php
@@ -26,256 +26,261 @@ namespace Dreamscapes;
 class Enumeration
 {
 
-  private static $instances = [];   // Instances of enumeration members are cached here
+    private static $instances = [];   // Instances of enumeration members are cached here
 
 
-  private $value;                   // Each instance of an Enumeration holds the member's value here
+    private $value;                   // Each instance of an Enumeration holds the member's value here
 
 
-  /**
-   * Instances are not allowed to be created by users
-   *
-   * @param     string      $name       The enumerated member's name
-   * @param     mixed       $value      The enumerated member's value
-   */
-  final private function __construct( $name, $value )
-  {
-    $this->name   = $name;
-    $this->value  = $value;
-  }
-
-  /**
-   * Get the name of the member that holds given value
-   *
-   * <p class="alert">This method is type-sensitive - see the example below.</p>
-   * <h3>Use case</h3>
-   * You have a value that is defined in an enumeration and you would like
-   * to know the name of the enumeration member that holds this value.
-   *
-   * <h3>Example</h3>
-   * <code>
-   * class Animal extends Dreamscapes\Enumeration
-   * {
-   *   const Horse = 0;
-   *   const Dog = 1;
-   * }
-   *
-   * echo Animal::getName( 0 ); // Prints 'Horse'
-   * echo Animal::getName( '0' ); // Returns null, method is type-sensitive
-   * </code>
-   *
-   * @param     string      $value     The member's expected value. <b>The
-   *                                   value must be of the same type as defined
-   *                                   in the Enumeration.</b>
-   *
-   * @return    string      The name of the member that holds this value, null
-   *                        if no such member exists
-   */
-  public static function getName( $value )
-  {
-    $key = array_search( $value, static::toArray(), true );  // Search using strict comparison
-    if ( $key === false ) static::triggerUndefinedConstantError( $value );
-
-    return $key;
-  }
-
-  /**
-   * Get the value of a given member's name
-   *
-   * <h3>Use case</h3>
-   * You have a string representation of the Enumeration member and you would
-   * like to know the value that member holds.
-   *
-   * <h3>Example</h3>
-   * <code>
-   * class Animal extends Dreamscapes\Enumeration
-   * {
-   *   const Horse = 0;
-   *   const Dog = 1;
-   * }
-   *
-   * echo Animal::getValue( 'Dog' ); // Prints an integer, 1
-   * </code>
-   *
-   * @param     string|Enumeration      $member     The member's expected name
-   *
-   * @return    bool                                The value of the member
-   */
-  public static function getValue( $member )
-  {
-    // Typecast to string (we could be getting either a string or an instance of Enumeration in $member)
-    $member = (string)$member;
-    ( ! static::isDefined( $member ) ) && static::triggerUndefinedConstantError( $member );
-
-    return static::toArray()[$member];
-  }
-
-  /**
-   * Does a member with this name exist in the enumeration?
-   *
-   * <h3>Example</h3>
-   * <code>
-   * class Animal extends Dreamscapes\Enumeration
-   * {
-   *   const Horse = 0;
-   *   const Dog = 1;
-   * }
-   *
-   * echo Animal::isDefined( 'Dog' ); // Prints an integer, 1 (bool true)
-   * echo Animal::isDefined( 'Cat' ); // Prints nothing (bool false)
-   * </code>
-   *
-   * @param     string      $member     The member's expected name
-   *
-   * @return    bool                    <b>true</b> if such member is defined, <b>false</b> otherwise
-   */
-  public static function isDefined( $member )
-  {
-    return array_key_exists( (string)$member, static::toArray() );
-  }
-
-  /**
-   * Get all members defined in this Enumeration
-   *
-   * <p class="alert">The returned array's order is determined by the order
-   * in which the constants are defined in the class.</p>
-   *
-   * <h3>Example</h3>
-   * <code>
-   * class Animal extends Dreamscapes\Enumeration
-   * {
-   *   const Horse = 0;
-   *   const Dog = 1;
-   * }
-   *
-   * print_r( Animal::allMembers() );
-   * // Array
-   * // (
-   * //   0 => 'Horse'
-   * //   1 => 'Dog'
-   * // )
-   * </code>
-   *
-   * @return    array     An ordered list of all Enumeration members
-   */
-  public static function allMembers()
-  {
-    return array_keys( static::toArray() );
-  }
-
-  /**
-   * Convert the Enumeration into an array
-   *
-   * <h3>Example</h3>
-   * <code>
-   * class Animal extends Dreamscapes\Enumeration
-   * {
-   *   const Horse = 0;
-   *   const Dog = 1;
-   * }
-   *
-   * print_r( Animal::allMembers() );
-   * // Array
-   * // (
-   * //   'Horse' => 0
-   * //   'Dog' => 1
-   * // )
-   * </code>
-   *
-   * @return    array
-   */
-  public static function toArray()
-  {
-    $enumClass = new \ReflectionClass( get_called_class() );
-
-    return $enumClass->getConstants();
-  }
-
-  /**
-   * Get the string representation of the Enumeration, without namespace
-   *
-   * <h3>Example</h3>
-   * <code>
-   * namespace Fauna;
-   *
-   * class Animal extends Dreamscapes\Enumeration {}
-   *
-   * echo Animal::getType(); // Animal
-   * echo Dreamscapes\Enumeration::getType(); // Enumeration
-   * </code>
-   *
-   * @return    string      The name of the Enumeration class, without namespace
-   */
-  public static function getType()
-  {
-    $type = get_called_class();
-
-    return end( explode( "\\", $type ) );
-  }
-
-  /**
-   * Maps static method calls to defined enumeration members
-   *
-   * @internal
-   *
-   * @return    Enumeration     Instance of the Enumeration subclass
-   */
-  public static function __callStatic( $method, $args )
-  {
-    return static::getMemberInstance( get_called_class(), $method );
-  }
-
-  /**
-   * Get the value of the enumerated member represented by this instance
-   *
-   * @return    mixed         Value of the enumerated member
-   */
-  final public function value()
-  {
-    return $this->value;
-  }
-
-  /**
-   * Allow enumeration members to be typecast to strings
-   *
-   * @return    string      The value of the enumeration member in a string representation
-   */
-  public function __toString()
-  {
-    return (string)$this->name;
-  }
-
-  /**
-   * Factory for enumeration members' instance representations
-   *
-   * @param     string        $enumeration    The class for which the instance should be obtained/generated
-   * @param     string        $member         The enumerated member's name to retrieve
-   *
-   * @return    Enumeration   An instance of the Enumeration subclass, representing the given member's value
-   */
-  final private static function getMemberInstance( $enumeration, $member )
-  {
-    if ( ! isset( self::$instances[$enumeration] ) || ! isset( self::$instances[$enumeration][$member] ) )
+    /**
+     * Instances are not allowed to be created by users
+     *
+     * @param     string      $name       The enumerated member's name
+     * @param     mixed       $value      The enumerated member's value
+     */
+    final private function __construct($name, $value)
     {
-      // Instance of this enumeration member does not exist yet - let's create one
-      self::$instances[$enumeration][$member] = new $enumeration( $member, static::getValue( $member ) );
+        $this->name = $name;
+        $this->value = $value;
     }
 
-    return self::$instances[$enumeration][$member];
-  }
+    /**
+     * Get the name of the member that holds given value
+     *
+     * <p class="alert">This method is type-sensitive - see the example below.</p>
+     * <h3>Use case</h3>
+     * You have a value that is defined in an enumeration and you would like
+     * to know the name of the enumeration member that holds this value.
+     *
+     * <h3>Example</h3>
+     * <code>
+     * class Animal extends Dreamscapes\Enumeration
+     * {
+     *   const Horse = 0;
+     *   const Dog = 1;
+     * }
+     *
+     * echo Animal::getName(0); // Prints 'Horse'
+     * echo Animal::getName('0'); // Returns null, method is type-sensitive
+     * </code>
+     *
+     * @param     string      $value     The member's expected value. <b>The
+     *                                   value must be of the same type as defined
+     *                                   in the Enumeration.</b>
+     *
+     * @return    string      The name of the member that holds this value, null
+     *                        if no such member exists
+     */
+    public static function getName($value)
+    {
+        $key = array_search($value, static::toArray(), true);  // Search using strict comparison
 
-  /**
-   * Trigger an error that php triggers when attempting to access undefined class constant
-   *
-   * @param     string        $const        The name of the constant being accessed
-   */
-  final private static function triggerUndefinedConstantError( $const )
-  {
-    $trace = debug_backtrace();
-      trigger_error(
-        'Undefined class constant ' . $const .
-        ' in ' . $trace[1]['file'] .
-        ' on line ' . $trace[1]['line'],
-        E_USER_ERROR );
-  }
+        if ($key === false) {
+            static::triggerUndefinedConstantError($value);
+        }
+
+        return $key;
+    }
+
+    /**
+     * Get the value of a given member's name
+     *
+     * <h3>Use case</h3>
+     * You have a string representation of the Enumeration member and you would
+     * like to know the value that member holds.
+     *
+     * <h3>Example</h3>
+     * <code>
+     * class Animal extends Dreamscapes\Enumeration
+     * {
+     *   const Horse = 0;
+     *   const Dog = 1;
+     * }
+     *
+     * echo Animal::getValue('Dog'); // Prints an integer, 1
+     * </code>
+     *
+     * @param     string|Enumeration      $member     The member's expected name
+     *
+     * @return    bool                                The value of the member
+     */
+    public static function getValue($member)
+    {
+        // Typecast to string (we could be getting either a string or an instance of Enumeration in $member)
+        $member = (string)$member;
+        if (! static::isDefined($member)) {
+            static::triggerUndefinedConstantError($member);
+        }
+
+        return static::toArray()[$member];
+    }
+
+    /**
+     * Does a member with this name exist in the enumeration?
+     *
+     * <h3>Example</h3>
+     * <code>
+     * class Animal extends Dreamscapes\Enumeration
+     * {
+     *   const Horse = 0;
+     *   const Dog = 1;
+     * }
+     *
+     * echo Animal::isDefined('Dog'); // Prints an integer, 1 (bool true)
+     * echo Animal::isDefined('Cat'); // Prints nothing (bool false)
+     * </code>
+     *
+     * @param     string      $member     The member's expected name
+     *
+     * @return    bool                    <b>true</b> if such member is defined, <b>false</b> otherwise
+     */
+    public static function isDefined($member)
+    {
+        return array_key_exists((string)$member, static::toArray());
+    }
+
+    /**
+     * Get all members defined in this Enumeration
+     *
+     * <p class="alert">The returned array's order is determined by the order
+     * in which the constants are defined in the class.</p>
+     *
+     * <h3>Example</h3>
+     * <code>
+     * class Animal extends Dreamscapes\Enumeration
+     * {
+     *   const Horse = 0;
+     *   const Dog = 1;
+     * }
+     *
+     * print_r(Animal::allMembers());
+     * // Array
+     * // (
+     * //   0 => 'Horse'
+     * //   1 => 'Dog'
+     * // )
+     * </code>
+     *
+     * @return    array     An ordered list of all Enumeration members
+     */
+    public static function allMembers()
+    {
+        return array_keys(static::toArray());
+    }
+
+    /**
+     * Convert the Enumeration into an array
+     *
+     * <h3>Example</h3>
+     * <code>
+     * class Animal extends Dreamscapes\Enumeration
+     * {
+     *   const Horse = 0;
+     *   const Dog = 1;
+     * }
+     *
+     * print_r(Animal::toArray());
+     * // Array
+     * // (
+     * //   'Horse' => 0
+     * //   'Dog' => 1
+     * // )
+     * </code>
+     *
+     * @return    array
+     */
+    public static function toArray()
+    {
+        $enumClass = new \ReflectionClass(get_called_class());
+
+        return $enumClass->getConstants();
+    }
+
+    /**
+     * Get the string representation of the Enumeration, without namespace
+     *
+     * <h3>Example</h3>
+     * <code>
+     * namespace Fauna;
+     *
+     * class Animal extends \Dreamscapes\Enumeration {}
+     *
+     * echo Animal::getType(); // Animal
+     * echo \Dreamscapes\Enumeration::getType(); // Enumeration
+     * </code>
+     *
+     * @return    string      The name of the Enumeration class, without namespace
+     */
+    public static function getType()
+    {
+        $type = get_called_class();
+
+        return end(explode("\\", $type));
+    }
+
+    /**
+     * Maps static method calls to defined enumeration members
+     *
+     * @internal
+     *
+     * @return    Enumeration     Instance of the Enumeration subclass
+     */
+    public static function __callStatic($method, $args)
+    {
+        return static::getMemberInstance(get_called_class(), $method);
+    }
+
+    /**
+     * Get the value of the enumerated member represented by this instance
+     *
+     * @return    mixed         Value of the enumerated member
+     */
+    final public function value()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Allow enumeration members to be typecast to strings
+     *
+     * @return    string      The value of the enumeration member in a string representation
+     */
+    public function __toString()
+    {
+        return (string)$this->name;
+    }
+
+    /**
+     * Factory for enumeration members' instance representations
+     *
+     * @param     string        $enumeration    The class for which the instance should be obtained/generated
+     * @param     string        $member         The enumerated member's name to retrieve
+     *
+     * @return    Enumeration   An instance of the Enumeration subclass, representing the given member's value
+     */
+    final private static function getMemberInstance($enumeration, $member)
+    {
+        if (! isset(self::$instances[$enumeration]) || ! isset(self::$instances[$enumeration][$member])) {
+            // Instance of this enumeration member does not exist yet - let's create one
+            self::$instances[$enumeration][$member] = new $enumeration($member, static::getValue($member));
+        }
+
+        return self::$instances[$enumeration][$member];
+    }
+
+    /**
+     * Trigger an error that php triggers when attempting to access undefined class constant
+     *
+     * @param     string        $const        The name of the constant being accessed
+     */
+    final private static function triggerUndefinedConstantError($const)
+    {
+        $trace = debug_backtrace();
+        trigger_error(
+            'Undefined class constant ' . $const .
+            ' in ' . $trace[1]['file'] .
+            ' on line ' . $trace[1]['line'],
+            E_USER_ERROR
+        );
+    }
 }
--- tests/unit/EnumerationTest.php
+++ tests/unit/EnumerationTest.php
@@ -1,137 +1,140 @@
 <?php
+namespace Dreamscapes;
+
 use Codeception\Util\Stub;
 
+// @codingStandardsIgnoreStart
+
 // Prepare a test Enumeration
-class TestEnum extends Dreamscapes\Enumeration
+class TestEnum extends Enumeration
 {
-  const TestMember  = 0;
-  const OtherMember = 1;
-  const FalseMember = false;
-  const TrueMember  = true;
+    const TestMember  = 0;
+    const OtherMember = 1;
+    const FalseMember = false;
+    const TrueMember  = true;
 }
 
-class AnotherEnum extends Dreamscapes\Enumeration
+class AnotherEnum extends Enumeration
 {
-  const DifferentMember = 'some value';
+    const DifferentMember = 'some value';
 }
+
+// @codingStandardsIgnoreEnd
 
 
 class EnumerationTest extends \Codeception\TestCase\Test
 {
-  protected $codeGuy;
-
-  protected function _before() {}
-  protected function _after() {}
+    protected $codeGuy;
 
 
-  public function testEnumerationsCannotBeInstantiated()
-  {
-    $refl  = new ReflectionClass( 'TestEnum' );
+    public function testEnumerationsCannotBeInstantiated()
+    {
+        $refl  = new \ReflectionClass('Dreamscapes\TestEnum');
 
-    $this->assertFalse( $refl->getConstructor()->isPublic() );
-    $this->assertTrue( $refl->getConstructor()->isFinal() );
-  }
+        $this->assertFalse($refl->getConstructor()->isPublic());
+        $this->assertTrue($refl->getConstructor()->isFinal());
+    }
 
-  public function testEachEnumerationMustBeIsolated()
-  {
-    $this->assertSame( 'some value', AnotherEnum::getValue( 'DifferentMember') );
-    $this->assertSame( 1, TestEnum::getValue( 'OtherMember' ) );
+    public function testEachEnumerationMustBeIsolated()
+    {
+        $this->assertSame('some value', AnotherEnum::getValue('DifferentMember'));
+        $this->assertSame(1, TestEnum::getValue('OtherMember'));
 
-    $this->setExpectedException( 'ErrorException' );
-    TestEnum::getValue( 'DifferentMember' );
-  }
+        $this->setExpectedException('ErrorException');
+        TestEnum::getValue('DifferentMember');
+    }
 
-  public function testAccessingUndefinedEnumerationMemberShouldTriggerError()
-  {
-    $this->setExpectedException( 'ErrorException' );
-    TestEnum::NonExistent();
-  }
+    public function testAccessingUndefinedEnumerationMemberShouldTriggerError()
+    {
+        $this->setExpectedException('ErrorException');
+        TestEnum::NonExistent();
+    }
 
-  public function testEnumerationCanTranslateValueToMemberName()
-  {
-    $this->assertEquals( 'TestMember', TestEnum::getName( 0 ) );
-  }
+    public function testEnumerationCanTranslateValueToMemberName()
+    {
+        $this->assertEquals('TestMember', TestEnum::getName(0));
+    }
 
-  public function testGetNameMethodIsTypeSensitive()
-  {
-    $this->assertSame( 'TestMember', TestEnum::getName( 0 ) );
-    $this->assertSame( 'FalseMember', TestEnum::getName( false ) );
-  }
+    public function testGetNameMethodIsTypeSensitive()
+    {
+        $this->assertSame('TestMember', TestEnum::getName(0));
+        $this->assertSame('FalseMember', TestEnum::getName(false));
+    }
 
-  public function testEnumerationCanReturnMembersViaMethodCall()
-  {
-    $this->assertSame( 1, TestEnum::getValue( 'OtherMember' ) );
-    $this->assertSame( false, TestEnum::getValue( 'FalseMember' ) );
-  }
+    public function testEnumerationCanReturnMembersViaMethodCall()
+    {
+        $this->assertSame(1, TestEnum::getValue('OtherMember'));
+        $this->assertSame(false, TestEnum::getValue('FalseMember'));
+    }
 
-  public function testMemberExistenceMethod()
-  {
-    $this->assertTrue( TestEnum::isDefined( 'TestMember' ) );
-    $this->assertTrue( TestEnum::isDefined( 'FalseMember' ) );
+    public function testMemberExistenceMethod()
+    {
+        $this->assertTrue(TestEnum::isDefined('TestMember'));
+        $this->assertTrue(TestEnum::isDefined('FalseMember'));
 
-    $this->assertFalse( TestEnum::isDefined( 'ImaginaryMember' ) );
-  }
+        $this->assertFalse(TestEnum::isDefined('ImaginaryMember'));
+    }
 
-  public function testRetrievingAllMembersShouldReturnOrderedListOfMembers()
-  {
-    $expected = [
-      'TestMember',
-      'OtherMember',
-      'FalseMember',
-      'TrueMember',
-    ];
+    public function testRetrievingAllMembersShouldReturnOrderedListOfMembers()
+    {
+        $expected = [
+            'TestMember',
+            'OtherMember',
+            'FalseMember',
+            'TrueMember',
+        ];
 
-    $this->assertSame( $expected, TestEnum::allMembers() );
-  }
+        $this->assertSame($expected, TestEnum::allMembers());
+    }
 
-  public function testGetTypeReturnsOnlyClassName()
-  {
-    $this->assertSame( 'TestEnum', TestEnum::getType() );
-    $this->assertSame( 'AnotherEnum', AnotherEnum::getType() );
-  }
+    public function testGetTypeReturnsOnlyClassName()
+    {
+        $this->assertSame('TestEnum', TestEnum::getType());
+        $this->assertSame('AnotherEnum', AnotherEnum::getType());
+    }
 
-  public function testEnumerationMembersAreActuallyCallable()
-  {
-    $this->assertTrue( is_callable( 'TestEnum', 'TestMember' ) );
-  }
+    public function testEnumerationMembersAreActuallyCallable()
+    {
+        $this->assertTrue(is_callable('TestEnum', 'TestMember'));
+    }
 
-  public function testCallingEnumerationMemberReturnsInstanceOfTheEnumeration()
-  {
-    $this->assertInstanceOf( 'TestEnum', TestEnum::TestMember() );
-  }
+    public function testCallingEnumerationMemberReturnsInstanceOfTheEnumeration()
+    {
+        $this->assertInstanceOf('Dreamscapes\TestEnum', TestEnum::TestMember());
+    }
 
-  public function testOnlySingleInstanceForEachEnumeratedMemberExists()
-  {
-    $first  = TestEnum::TestMember();
-    $second = TestEnum::TestMember();
+    public function testOnlySingleInstanceForEachEnumeratedMemberExists()
+    {
+        $first  = TestEnum::TestMember();
+        $second = TestEnum::TestMember();
 
-    $this->assertSame( $first, $second );
-  }
+        $this->assertSame($first, $second);
+    }
 
-  public function testInstancesOfEnumerationMembersCanBeUsedAsStrings()
-  {
-    $this->assertEquals( 'TrueMember', (string)TestEnum::TrueMember() );
-    $this->assertEquals( 'FalseMember', (string)TestEnum::FalseMember() );
-    $this->assertEquals( 'TestMember', (string)TestEnum::TestMember() );
-  }
+    public function testInstancesOfEnumerationMembersCanBeUsedAsStrings()
+    {
+        $this->assertEquals('TrueMember', (string)TestEnum::TrueMember());
+        $this->assertEquals('FalseMember', (string)TestEnum::FalseMember());
+        $this->assertEquals('TestMember', (string)TestEnum::TestMember());
+    }
 
-  public function testGetValueMethodAcceptsInstanceOfEnumeration()
-  {
-    $instance = TestEnum::TestMember();
+    public function testGetValueMethodAcceptsInstanceOfEnumeration()
+    {
+        $instance = TestEnum::TestMember();
 
-    $this->assertSame( 0, TestEnum::getValue( $instance ) );
-  }
+        $this->assertSame(0, TestEnum::getValue($instance));
+    }
 
-  public function testIsDefinedMethodAcceptsInstanceOfEnumeration()
-  {
-    $instance = TestEnum::TestMember();
+    public function testIsDefinedMethodAcceptsInstanceOfEnumeration()
+    {
+        $instance = TestEnum::TestMember();
 
-    $this->assertSame( true, TestEnum::isDefined( $instance ) );
-  }
+        $this->assertSame(true, TestEnum::isDefined($instance));
+    }
 
-  public function testInstancesHaveValueMethod()
-  {
-    $instance = TestEnum::TestMember();
-    $this->assertSame( TestEnum::TestMember, $instance->value() );
-  }
+    public function testInstancesHaveValueMethod()
+    {
+        $instance = TestEnum::TestMember();
+        $this->assertSame(TestEnum::TestMember, $instance->value());
+    }
 }
